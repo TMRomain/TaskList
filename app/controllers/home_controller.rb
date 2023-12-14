84703012bd9df999ef6dd053ff45f603c1801b79
TMRomain <romainover51@gmail.com>
2023-12-14 20:21:05 +0100

    Ajout logique pour modifier tache dans controller #2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,14 @@ class HomeController < ApplicationController
   end
 
   def create
+    #check if params[:id is not null]
+    #if not null, update the task
+    #else create a new task
+    if params[:id] != ""
+      updateTask
+      return
+    end
+
     task_title = params[:title]
     task_description = params[:description]
     task_date = params[:date]
@@ -21,14 +29,25 @@ class HomeController < ApplicationController
     redirect_to '/'
   end
 
+  def updateTask
+    task = Task.find(params[:id])
+    task.title = params[:title]
+    task.desc = params[:description]
+    task.date = params[:date]
+    task.iscompleted = params[:iscompleted]
+    task.save
+    redirect_to '/'
+  end
+
   def delete
     task = Task.find(params[:id])
     task.destroy
     redirect_to '/'
   end
 
-  def update
+  def redirectToUpdate
     @task = Task.find(params[:id])
+    # cosole.log(@task)
     # load view updateTask.html.erb
     render template: "home/updateTask"
   end
